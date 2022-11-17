87ed2498f3ab4081917d1f8e2b1c2b793acafa4c
M.Horbacz <maciej.horbacz@gmail.com>
2022-11-17 13:08:30 +0000

    🎉 convert sc to custom!

---
+++ ConvertFrom-SettingsCatalog.ps1
@@ -0,0 +1,19 @@
+﻿function ConvertFrom-SettingsCatalog {
+[cmdletbinding()]
+param(
+	# Auth to MS Graph
+	[Parameter(Mandatory)]
+	[hashtable]
+	$AuthToken,
+	# Settings Catalog profile name
+	[Parameter(Mandatory)]
+	[string[]]
+	$ProfileName,
+	# Settings Catalog profile id
+	[Parameter()]
+	[stringp[]]
+	$ProfileId
+)
+
+#
+}
